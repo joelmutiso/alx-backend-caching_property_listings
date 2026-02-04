121a6a937e5cf102d71dfbcc574cdc49c673a33d
Joel Mutiso <joelwmutiso@gmail.com>
2026-02-04 20:54:31 +0300

    feat: implement cache invalidation using django signals

--- properties/apps.py
+++ properties/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
-
 class PropertiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        # Importing signals inside ready() ensures they are registered 
+        # as soon as the app registry is fully populated.
+        import properties.signals
